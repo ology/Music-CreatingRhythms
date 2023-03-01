196d6b95629675c4d00cda8bf1a5f70244a79db1
Gene Boggs <gene@ology.net>
2023-03-01 15:38:55 -0800

    Improve!

--- eg/pfold-set.pl
+++ eg/pfold-set.pl
@@ -7,11 +7,13 @@ use warnings;
 use Data::Dumper::Compact qw(ddc);
 use Music::CreatingRhythms ();
 
+my $n = shift || 16;  # number of terms
 my $m = shift || 16;  # maximum iteration
+my $f = shift || 4;   # folding function number 0 to (2^m)-1
 
 my $mcr = Music::CreatingRhythms->new;
 
 for my $i (0 .. $m - 1) {
-    my $sequence = $mcr->pfold(15, 4, $i);
+    my $sequence = $mcr->pfold($n, $f, $i);
     print ddc($sequence, {max_width=>128});
 }
