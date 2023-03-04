1278efbb609a079a40b625c184907cd78767574c
Gene Boggs <gene@ology.net>
2023-03-03 21:06:58 -0800

    Initial commit

---
+++ eg/play-set.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+# Play Christoffel word sets.
+
+use Data::Dumper::Compact qw(ddc);
+use lib map { "$ENV{HOME}/sandbox/$_/lib" } qw(MIDI-Drummer-Tiny Music-CreatingRhythms);
+use MIDI::Drummer::Tiny ();
+use Music::CreatingRhythms ();
+
+my $t = shift || 'u'; # type of word: u=upper, l=lower
+my $p = shift || 2;   # numerator of slope
+my $m = shift || 14;  # maximum denominator
+my $n = shift || 16;  # number of terms to generate
+
+my $max = shift || 4; # times to loop
+
+my $mcr = Music::CreatingRhythms->new;
+
+my $d = MIDI::Drummer::Tiny->new(
+   file   => 'play-set.mid',
+   bpm    => 100,
+   volume => 100,
+   bars   => 4,
+   reverb => 15,
+);
+
+$d->sync(
+    \&hihat,
+    \&snare_drum,
+    \&kick_drum,
+);
+
+$d->write;
+
+sub hihat {
+    my $q = int rand $m;
+    my $sequence = [ (1) x 8 ];
+    print 'Hihat: ', ddc($sequence, {max_width=>128});
+    for (1 .. $max / 2) {
+        for my $i (@$sequence) {
+            $i ? $d->note('en', $d->closed_hh) : $d->rest('en');
+        }
+    }
+}
+
+sub snare_drum {
+    my $q = int rand $m;
+    my $sequence = $mcr->chsequl($t, $p, $q, $n);
+    print 'Snare: ', ddc($sequence, {max_width=>128});
+    for (1 .. $max) {
+        for my $i (@$sequence) {
+            $i ? $d->note('sn', $d->snare) : $d->rest('sn');
+        }
+    }
+}
+
+sub kick_drum {
+    my $q = int rand $m;
+    my $sequence = $mcr->chsequl($t, $p, $q, $n);
+    print 'Kick:  ', ddc($sequence, {max_width=>128});
+    for (1 .. $max) {
+        for my $i (@$sequence) {
+            $i ? $d->note('sn', $d->kick) : $d->rest('sn');
+        }
+    }
+}
