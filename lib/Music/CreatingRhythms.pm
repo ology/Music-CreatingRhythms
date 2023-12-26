3b14a98001e93355bdb6327c434180a409ac7daa
Gene Boggs <gene@ology.net>
2023-12-26 14:36:29 -0800

    Bump version

--- lib/Music/CreatingRhythms.pm
+++ lib/Music/CreatingRhythms.pm
@@ -2,7 +2,7 @@ package Music::CreatingRhythms;
 
 # ABSTRACT: Combinatorial algorithms to generate rhythms
 
-our $VERSION = '0.0804';
+our $VERSION = '0.0805';
 
 use strictures 2;
 use Algorithm::Combinatorics qw(permutations);
